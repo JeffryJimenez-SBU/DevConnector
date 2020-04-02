f9711473a8d0d8e77753fe1f7baa252fe542c855
JeffryJimenez-SBU <jeffry.jimenez@stonybrook.edu>
2020-04-02 14:11:04 -0400

    User, auth, profile, posts routes created

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
 
+//define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
