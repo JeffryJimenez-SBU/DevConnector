5377ec0f62eba08cd40e9ee40af2eedf58d8cdb7
JeffryJimenez-SBU <jeffry.jimenez@stonybrook.edu>
2020-04-03 00:09:13 -0400

    Profile router functional

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 const gravatar = require("gravatar");
 
@@ -59,9 +61,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt); //cretaes a hass for the password
 
       await user.save();
-      //return jsonwebtoken
 
-      res.send("User Registered");
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("server error");
