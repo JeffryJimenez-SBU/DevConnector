d54fb912f8a10585cdbe008492a0c6a2ed3d555f
JeffryJimenez-SBU <jeffry.jimenez@stonybrook.edu>
2020-04-04 18:15:02 -0400

    user loggin works, redictercts to /dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -43,6 +45,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
@@ -51,5 +54,32 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       });
     }
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// //Login User user
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const body = JSON.stringify({ email, password });
+  try {
+    const res = await axios.post("/api/auth", body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err) => {
+        dispatch(setAlert(err.msg, "danger"));
+      });
+    }
+    dispatch({ type: LOGIN_FAIL });
   }
 };
